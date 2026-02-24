3a2c5df21965721ce9f4f09be40512250c86c616
ayoub akraou <ayoubakraou@gmail.com>
2026-02-24 14:38:56 +0000

    test(auth): test register functionality

---
+++ backend/src/auth/auth.service.spec.ts
@@ -0,0 +1,79 @@
+import { Test, TestingModule } from "@nestjs/testing"
+import { AuthService } from "./auth.service.js"
+import { PrismaService } from "../prisma/prisma.service.js"
+import { jest } from '@jest/globals';
+import { UserRole } from "@prisma/client";
+
+type FindUnique = { id: string } | null;
+type CreateUser = {
+    id: string;
+    name: string;
+    email: string;
+    role: UserRole;
+    createdAt: Date;
+}
+
+describe('AuthService', () => {
+    let service: AuthService
+
+    const prismaMock = {
+        user: {
+            findUnique: jest.fn<() => Promise<FindUnique>>(),
+            create: jest.fn<() => Promise<CreateUser>>()
+        }
+    }
+
+    beforeEach(async () => {
+        jest.clearAllMocks();
+
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [AuthService, {
+                provide: PrismaService,
+                useValue: prismaMock
+            }],
+        }).compile()
+
+        service = module.get<AuthService>(AuthService);
+    })
+
+    it('should be defined', () => {
+        expect(service).toBeDefined();
+    })
+
+    it('should register a new user', async () => {
+        const dto = {
+            name: '  John Doe  ',
+            email: '  JOHN@EXAMPLE.COM  ',
+            password: 'password123',
+        };
+
+        prismaMock.user.findUnique.mockResolvedValue(null)
+
+        prismaMock.user.create.mockResolvedValue(
+            {
+                id: 'user_1',
+                name: 'John Doe',
+                email: 'john@example.com',
+                role: 'CUSTOMER',
+                createdAt: new Date('2026-01-01T00:00:00.000Z'),
+            }
+        )
+
+        const result = await service.register(dto)
+
+        expect(prismaMock.user.findUnique).toHaveBeenCalledWith({
+            where: { email: 'john@example.com' },
+            select: { id: true }
+        })
+
+        expect(prismaMock.user.create).toHaveBeenCalled()
+
+        expect(result).toMatchObject({
+            id: 'user_1',
+            name: 'John Doe',
+            email: 'john@example.com',
+            role: 'CUSTOMER',
+        })
+    })
+
+})
