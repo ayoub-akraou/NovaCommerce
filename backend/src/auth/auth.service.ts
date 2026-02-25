109a734c9ac6667609aa3331891de29542d6b232
ayoub akraou <ayoubakraou@gmail.com>
2026-02-25 15:28:32 +0000

    feat: add login functionality in auth service NOV-22

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,23 +1,25 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service.js';
 import { RegisterDto } from './dto/register.dto.js';
-import { hash } from 'bcryptjs';
+import { compare, hash } from 'bcryptjs';
 import { UserRole } from '@prisma/client';
+import { LoginDto } from './dto/login.dto.js';
+import { JwtService } from '@nestjs/jwt';
 
 
 @Injectable()
 export class AuthService {
-    constructor(private readonly prisma: PrismaService) {}
+    constructor(private readonly prisma: PrismaService, private readonly jwtService: JwtService) { }
 
     async register(dto: RegisterDto) {
         const normalizedEmail = dto.email.trim().toLowerCase();
 
         const existingUser = await this.prisma.user.findUnique({
-            where: {email: normalizedEmail},
-            select: {id: true}
+            where: { email: normalizedEmail },
+            select: { id: true }
         })
 
-        if(existingUser) throw new ConflictException('Email already in use.')
+        if (existingUser) throw new ConflictException('Email already in use.')
 
         const passwordHash = await hash(dto.password, 10)
 
@@ -37,4 +39,24 @@ export class AuthService {
             }
         })
     }
+
+    async login(dto: LoginDto) {
+        const normalizedEmail = dto.email.trim().toLowerCase();
+
+        const user = await this.prisma.user.findUnique({
+            where: { email: normalizedEmail },
+            select: { id: true, name: true, email: true, role: true, passwordHash: true },
+        });
+
+        if (!user) throw new UnauthorizedException('Invalid credentials.');
+
+        const isPasswordValid = await compare(dto.password, user.passwordHash);
+        if (!isPasswordValid) throw new UnauthorizedException('Invalid credentials.');
+
+        const payload = { sub: user.id, email: user.email, role: user.role }
+        const accessToken = await this.jwtService.signAsync(payload)
+
+        return { accessToken, id: user.id, name: user.name, email: user.email, role: user.role };
+    }
+
 }
