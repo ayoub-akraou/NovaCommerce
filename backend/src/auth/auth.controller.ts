28a9e9dd8d64459cf5019c0105da18ae04e6eaf3
ayoub akraou <ayoubakraou@gmail.com>
2026-02-25 15:38:30 +0000

    feat(auth): apply  throttler middleware on login route with more strict configuration

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,6 +1,8 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service.js';
 import { RegisterDto } from './dto/register.dto.js';
+import { LoginDto } from './dto/login.dto.js';
+import { Throttle } from '@nestjs/throttler';
 
 @Controller('auth')
 export class AuthController {
@@ -13,6 +15,7 @@ export class AuthController {
     }
 
     @Post('login')
+    @Throttle({default: {ttl: 60000, limit: 10}})
     @HttpCode(HttpStatus.OK)
     login(@Body() dto: LoginDto) {
         return this.authService.login(dto)
