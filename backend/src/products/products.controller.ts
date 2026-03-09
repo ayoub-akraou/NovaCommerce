c0408c7480e33e04b8d73f0e20090c942a1f40bd
ayoub akraou <ayoubakraou@gmail.com>
2026-03-09 14:30:57 +0000

    feat: secure products CRUD with admin roles

--- backend/src/products/products.controller.ts
+++ backend/src/products/products.controller.ts
@@ -6,15 +6,21 @@ import {
   Param,
   Patch,
   Post,
+  UseGuards,
 } from '@nestjs/common';
 import { ProductsService } from './products.service.js';
 import { CreateProductDto } from './dto/create-product.dto.js';
 import { UpdateProductDto } from './dto/update-product.dto.js';
+import { AuthGuard } from 'src/auth/guards/auth.guard.js';
+import { RolesGuard } from 'src/auth/guards/roles.guard.js';
+import { Roles } from 'src/auth/decorators/roles.decorator.js';
+import { UserRole } from '@prisma/client';
 
+@UseGuards(AuthGuard, RolesGuard)
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
-
+  @Roles(UserRole.ADMIN)
   @Post()
   create(@Body() dto: CreateProductDto) {
     return this.productsService.create(dto);
@@ -30,11 +36,13 @@ export class ProductsController {
     return this.productsService.findOne(id);
   }
 
+  @Roles(UserRole.ADMIN)
   @Patch(':id')
   update(@Param('id') id: string, @Body() dto: UpdateProductDto) {
     return this.productsService.update(id, dto);
   }
 
+  @Roles(UserRole.ADMIN)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.productsService.remove(id);
