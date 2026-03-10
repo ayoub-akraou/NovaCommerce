c2c0bd0643871183102f0bca88e610162acc84a9
ayoub akraou <ayoubakraou@gmail.com>
2026-03-10 14:03:35 +0000

    fix: add .js extensions

--- backend/src/cart/cart.module.ts
+++ backend/src/cart/cart.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
-import { CartController } from './cart.controller';
-import { CartService } from './cart.service';
+import { CartController } from './cart.controller.js';
+import { CartService } from './cart.service.js';
 
 @Module({
   controllers: [CartController],
-  providers: [CartService]
+  providers: [CartService],
 })
 export class CartModule {}
