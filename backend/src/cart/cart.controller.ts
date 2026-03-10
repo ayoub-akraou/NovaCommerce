1065d94a24e05ee7d917af149f180525a15495a1
ayoub akraou <ayoubakraou@gmail.com>
2026-03-10 15:17:16 +0000

    feat: add authenticated get cart endpoint

--- backend/src/cart/cart.controller.ts
+++ backend/src/cart/cart.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { CartService } from './cart.service.js';
+import { AuthGuard } from '../auth/guards/auth.guard.js';
 
 @Controller('cart')
-export class CartController {}
+@UseGuards(AuthGuard)
+export class CartController {
+  constructor(private readonly cartService: CartService) {}
+
+  @Get()
+  getMyCart(@Req() req: any) {
+    return this.cartService.getOrCreateUserCart(req.user.sub);
+  }
+}
