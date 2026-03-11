89c870691d1c0c6c6aa87aeaa94b56ff3130181d
ayoub akraou <ayoubakraou@gmail.com>
2026-03-11 12:02:32 +0000

    fix: add .js extension

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -10,7 +10,7 @@ import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
 import { CategoriesModule } from './categories/categories.module.js';
 import { ProductsModule } from './products/products.module.js';
-import { CartModule } from './cart/cart.module';
+import { CartModule } from './cart/cart.module.js';
 
 @Module({
   imports: [
