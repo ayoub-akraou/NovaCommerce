c1426738026f43a14ba81db03a4acefddc847dba
ayoub akraou <ayoubakraou@gmail.com>
2026-03-12 18:42:08 +0000

    fix: add .js extension

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
-import { UsersController } from './users.controller';
-import { UsersService } from './users.service';
+import { UsersController } from './users.controller.js';
+import { UsersService } from './users.service.js';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService],
 })
 export class UsersModule {}
