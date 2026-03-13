d325095705518cb8e2414253591e21daafce4fb1
ayoub akraou <ayoubakraou@gmail.com>
2026-03-13 10:04:33 +0000

    feat(admin): add updateRole functionality

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -19,4 +19,24 @@ export class UsersService {
     });
   }
 
+  async updateRole(userId: string, role: UserRole) {
+    const user = await this.prisma.user.findUnique({
+      where: { id: userId },
+      select: { id: true },
+    });
+
+    if (!user) throw new NotFoundException('User not found.');
+
+    return this.prisma.user.update({
+      where: { id: userId },
+      data: { role },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        role: true,
+        createdAt: true,
+      },
+    });
+  }
 }
