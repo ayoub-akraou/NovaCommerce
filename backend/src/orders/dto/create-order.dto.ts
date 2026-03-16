4d19b7b095062206397e0951cb0bc7dd72c0ae64
ayoub akraou <ayoubakraou@gmail.com>
2026-03-16 12:38:58 +0000

    docs(swagger): annotate create-order.dto.ts

--- backend/src/orders/dto/create-order.dto.ts
+++ backend/src/orders/dto/create-order.dto.ts
@@ -1,6 +1,8 @@
 import { IsString, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateOrderDto {
+  @ApiProperty({ example: '221B Baker Street, London', minLength: 5 })
   @IsString()
   @MinLength(5)
   address: string;
