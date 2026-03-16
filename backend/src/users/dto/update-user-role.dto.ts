7a0c902ebacd3cdd1b575fe97771736847f88c0b
ayoub akraou <ayoubakraou@gmail.com>
2026-03-16 12:39:00 +0000

    docs(swagger): annotate update-user-role.dto.ts

--- backend/src/users/dto/update-user-role.dto.ts
+++ backend/src/users/dto/update-user-role.dto.ts
@@ -1,7 +1,9 @@
 import { IsEnum } from 'class-validator';
 import { UserRole } from '@prisma/client';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateUserRoleDto {
+  @ApiProperty({ enum: UserRole, example: UserRole.ADMIN })
   @IsEnum(UserRole)
   role: UserRole;
 }
