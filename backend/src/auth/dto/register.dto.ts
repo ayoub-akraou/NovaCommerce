903e710c7e5e4e21e4a23a78fda7fe3d67b77b61
ayoub akraou <ayoubakraou@gmail.com>
2026-03-16 12:38:57 +0000

    docs(swagger): annotate register.dto.ts

--- backend/src/auth/dto/register.dto.ts
+++ backend/src/auth/dto/register.dto.ts
@@ -1,14 +1,18 @@
 import { IsEmail, IsString, MaxLength, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class RegisterDto {
+  @ApiProperty({ example: 'John Doe', minLength: 2, maxLength: 40 })
   @IsString()
   @MinLength(2)
   @MaxLength(40)
   name: string;
 
+  @ApiProperty({ example: 'john@example.com' })
   @IsEmail()
   email: string;
 
+  @ApiProperty({ example: 'StrongPass123!', minLength: 8, maxLength: 128 })
   @IsString()
   @MinLength(8)
   @MaxLength(128)
