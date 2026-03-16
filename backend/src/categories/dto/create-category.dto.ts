157a2c5ed5affed098a321394fc1e0906a98cd78
ayoub akraou <ayoubakraou@gmail.com>
2026-03-16 12:38:58 +0000

    docs(swagger): annotate create-category.dto.ts

--- backend/src/categories/dto/create-category.dto.ts
+++ backend/src/categories/dto/create-category.dto.ts
@@ -1,6 +1,8 @@
 import { IsString, MaxLength, MinLength } from "class-validator";
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateCategoryDto {
+    @ApiProperty({ example: 'Electronics', minLength: 2, maxLength: 80 })
     @IsString()
     @MinLength(2)
     @MaxLength(80)
