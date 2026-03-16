90f6890a9fd09840ff7dd17ba1ee2991ea44b0b9
ayoub akraou <ayoubakraou@gmail.com>
2026-03-16 12:38:59 +0000

    docs(swagger): annotate list-admin-orders-query.dto.ts

--- backend/src/orders/dto/list-admin-orders-query.dto.ts
+++ backend/src/orders/dto/list-admin-orders-query.dto.ts
@@ -1,22 +1,27 @@
 import { Type } from 'class-transformer';
 import { IsEnum, IsNumber, IsOptional, IsString, Min } from 'class-validator';
 import { OrderStatus } from '@prisma/client';
+import { ApiPropertyOptional } from '@nestjs/swagger';
 
 export class ListAdminOrdersQueryDto {
+  @ApiPropertyOptional({ enum: OrderStatus, example: OrderStatus.PAID })
   @IsOptional()
   @IsEnum(OrderStatus)
   status?: OrderStatus;
 
+  @ApiPropertyOptional({ example: 'user_123' })
   @IsOptional()
   @IsString()
   userId?: string;
 
+  @ApiPropertyOptional({ example: 1, minimum: 1, default: 1 })
   @IsOptional()
   @Type(() => Number)
   @IsNumber()
   @Min(1)
   page?: number = 1;
 
+  @ApiPropertyOptional({ example: 10, minimum: 1, default: 10 })
   @IsOptional()
   @Type(() => Number)
   @IsNumber()
