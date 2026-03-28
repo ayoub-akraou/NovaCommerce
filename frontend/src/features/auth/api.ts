2b16ba9e2e1e7d35bd36335e4654e619a6878f8c
ayoub akraou <ayoubakraou@gmail.com>
2026-03-28 18:31:00 +0100

    feat(auth): add client register function

--- frontend/src/features/auth/api.ts
+++ frontend/src/features/auth/api.ts
@@ -6,3 +6,8 @@ export async function login(payload: LoginShemaInput): Promise<LoginResponse> {
 	const { data } = await apiClient.post<LoginResponse>("/auth/login", payload);
 	return data;
 }
+
+export async function register(payload: RegisterShemaInput): Promise<RegisterResponse> {
+	const { data } = await apiClient.post<RegisterResponse>("/auth/register", payload);
+	return data;
+}
