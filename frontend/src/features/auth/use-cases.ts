d591e91b2da84decad235f3d704d387dddedab43
ayoub akraou <ayoubakraou@gmail.com>
2026-03-29 15:36:20 +0100

    feat(auth): return login result from use case

--- frontend/src/features/auth/use-cases.ts
+++ frontend/src/features/auth/use-cases.ts
@@ -1,11 +1,12 @@
 import { useAuthStore } from "@/store/auth.store";
 import { login, logout, mapAuthResponseToSession, refresh, register } from "./api";
-import { LoginSchemaInput, RegisterSchemaInput } from "./schema";
+import type { LoginSchemaInput, RegisterSchemaInput } from "./schema";
 
 export async function loginUseCase(payload: LoginSchemaInput) {
 	const data = await login(payload);
 	const session = mapAuthResponseToSession(data);
 	useAuthStore.getState().setSession(session);
+	return data;
 }
 
 export async function registerUseCase(payload: RegisterSchemaInput) {
