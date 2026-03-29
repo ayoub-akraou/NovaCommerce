40c86076b1ecc79854afbb58748f5229cf609220
ayoub akraou <ayoubakraou@gmail.com>
2026-03-29 09:00:56 +0100

    feat(auth): add register use case

--- frontend/src/features/auth/use-cases.ts
+++ frontend/src/features/auth/use-cases.ts
@@ -1,9 +1,13 @@
 import { useAuthStore } from "@/store/auth.store";
-import { login, mapAuthResponseToSession } from "./api";
-import { LoginSchemaInput } from "./schema";
+import { login, mapAuthResponseToSession, register } from "./api";
+import { LoginSchemaInput, RegisterSchemaInput } from "./schema";
 
 export async function loginUseCase(payload: LoginSchemaInput) {
 	const data = await login(payload);
 	const session = mapAuthResponseToSession(data);
 	useAuthStore.getState().setSession(session);
+}
+
+export async function registerUseCase(payload: RegisterSchemaInput) {
+	return register(payload);
 }
