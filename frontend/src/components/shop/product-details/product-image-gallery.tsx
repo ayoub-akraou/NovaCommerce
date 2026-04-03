04f705094ab5422320bc0dc9ae261187cd8f26ec
ayoub akraou <ayoubakraou@gmail.com>
2026-04-03 22:30:08 +0100

    feat(frontend): scaffold product image gallery component

---
+++ frontend/src/components/shop/product-details/product-image-gallery.tsx
@@ -0,0 +1,10 @@
+type ProductImageGalleryProps = {
+	title: string;
+	images: string[];
+	selectedImageIndex: number;
+	onSelectImage: (index: number) => void;
+};
+
+export function ProductImageGallery(_props: ProductImageGalleryProps) {
+	return null;
+}
