30bbde883b97268a5859d545513642ccbbcacc94
ayoub akraou <ayoubakraou@gmail.com>
2026-04-03 21:47:59 +0100

    feat(frontend): scaffold shop products use-cases layer

---
+++ frontend/src/features/shop/products/use-cases.ts
@@ -0,0 +1,10 @@
+﻿import { getShopCategories, getShopProducts } from "./api";
+import type { ListShopProductsQuery } from "./types";
+
+export async function listShopProductsUseCase(query: ListShopProductsQuery) {
+	return getShopProducts(query);
+}
+
+export async function listShopCategoriesUseCase() {
+	return getShopCategories();
+}
