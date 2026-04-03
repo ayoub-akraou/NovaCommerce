f43237fb91905dc08f2b77082c4cf2a245e59742
ayoub akraou <ayoubakraou@gmail.com>
2026-04-03 21:49:36 +0100

    feat(frontend): scaffold products pagination component

---
+++ frontend/src/components/shop/products/products-pagination.tsx
@@ -0,0 +1,10 @@
+﻿type ProductsPaginationProps = {
+	page: number;
+	totalPages: number;
+	onPrevious: () => void;
+	onNext: () => void;
+};
+
+export function ProductsPagination(_props: ProductsPaginationProps) {
+	return null;
+}
