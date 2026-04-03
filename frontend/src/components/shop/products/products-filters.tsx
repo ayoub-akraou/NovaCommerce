27c5f5ba2c04beb2356fad587c50f7eaacd55973
ayoub akraou <ayoubakraou@gmail.com>
2026-04-03 21:48:48 +0100

    feat(frontend): scaffold products filters component contract

---
+++ frontend/src/components/shop/products/products-filters.tsx
@@ -0,0 +1,13 @@
+﻿import type { ShopCategory, ShopProductsFiltersForm } from "@/features/shop/products/types";
+
+type ProductsFiltersProps = {
+	categories: ShopCategory[];
+	filters: ShopProductsFiltersForm;
+	onChange: (next: Partial<ShopProductsFiltersForm>) => void;
+	onApply: () => void;
+	onReset: () => void;
+};
+
+export function ProductsFilters(_props: ProductsFiltersProps) {
+	return null;
+}
